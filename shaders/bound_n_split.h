f7f6248e08dc1ef81ee98a77535f896a222063bb
Thomas Weber <ginko@cg.tuwien.ac.at>
2014-01-23 13:58:16 -0800

    Fixing problem with GL multipass bns refactor

--- shaders/bound_n_split.h
+++ shaders/bound_n_split.h
@@ -35,48 +35,26 @@ vec3 eval_patch(vec2 t, int patch_id)
 
 bool intersects_frustum(vec3 pmin, vec3 pmax)
 {
-    vec2 smin,smax;
+    
+    vec2 lmin,lmax;
 
     float n = -pmax.z;
     float f = -pmin.z;
 
     if (f < near || n > far) {
-        return false;
+        return true;
     }
 
     n = max(near, n);
 
-    if (pmin.x < 0) {
-        smin.x = pmin.x/n * proj_f.x + screen_size.x * 0.5;
-    } else {
-        smin.x = pmin.x/f * proj_f.x + screen_size.x * 0.5;
-    }
+    lmin.x = pmin.x/((pmin.x < 0) ? n : f) * proj_f.x + screen_size.x * 0.5;
+    lmin.y = pmin.y/((pmin.y < 0) ? n : f) * proj_f.y + screen_size.y * 0.5;
 
-    if (pmin.y < 0) {
-        smin.y = pmin.y/n * proj_f.y + screen_size.y * 0.5;
-    } else {
-        smin.y = pmin.y/f * proj_f.y + screen_size.y * 0.5;
-    }
+    lmax.x = pmax.x/((pmax.x > 0) ? n : f) * proj_f.x + screen_size.x * 0.5;
+    lmax.y = pmax.y/((pmax.y > 0) ? n : f) * proj_f.y + screen_size.y * 0.5;
 
-    if (pmax.x > 0) {
-        smax.x = pmax.x/n * proj_f.x + screen_size.x * 0.5;
-    } else {
-        smax.x = pmax.x/f * proj_f.x + screen_size.x * 0.5;
-    }
-
-    if (pmax.y > 0) {
-        smax.y = pmax.y/n * proj_f.y + screen_size.y * 0.5;
-    } else {
-        smax.y = pmax.y/f * proj_f.y + screen_size.y * 0.5;
-    }
-
-
-    if (smin.x > screen_size.x-1 + cull_ribbon || smax.x < -cull_ribbon ||
-        smin.y > screen_size.y-1 + cull_ribbon || smax.y < -cull_ribbon ){
-        return false;
-    }
-
-    return true;
+    return !(lmin.x > screen_size.x-1 + cull_ribbon || lmax.x < -cull_ribbon ||
+             lmin.y > screen_size.y-1 + cull_ribbon || lmax.y < -cull_ribbon );
 }
 
 
@@ -84,7 +62,7 @@ bool intersects_frustum(vec3 pmin, vec3 pmax)
 // A ... draw
 // B ... split
 // C ... split direction, 0=horizontal 1=vertical
-#define RES 3
+#define RES 4
 #define CULL 0
 #define DRAW 1
 #define HSPLIT 2
@@ -97,16 +75,15 @@ uint bound (uint rpid, vec2 rmin, vec2 rmax, uint rdepth)
     vec3 bbox_min = vec3( infinity);
     vec3 bbox_max = vec3(-infinity);
 
-    if (rdepth >= max_split_depth-1) return CULL;
+    uint depth_mask = (rdepth < max_split_depth-1) ? VSPLIT : 0;
     
-    for (uint u = 0; u < RES; ++u) {
-        for (uint v = 0; v < RES; ++v) {
-            vec2 uv = mix(rmin, rmax, vec2(u * (1.0f / (RES-1)), v * (1.0f / (RES-1))));
+    for (int v = 0; v < RES; ++v) {
+        for (int u = 0; u < RES; ++u) {
+            vec2 lt = vec2(u * (1.0f / (RES-1)), v * (1.0f / (RES-1)));
+            vec4 p = mv * vec4(eval_patch(mix(rmin,rmax,lt), int(rpid)),1);
 
-            vec4 p = mv * vec4(eval_patch(mix(rmin,rmax,uv), int(rpid)),1);
-
-            bbox_min = min(bbox_min, p.xyz);
-            bbox_max = max(bbox_max, p.xyz);
+            bbox_min = min(p.xyz, bbox_min);
+            bbox_max = max(p.xyz, bbox_max);
 
             vec4 pp = proj * p;
             ppos[v][u] = mat2(screen_matrix) * (pp.xy / pp.w);
@@ -116,9 +93,8 @@ uint bound (uint rpid, vec2 rmin, vec2 rmax, uint rdepth)
     if (!intersects_frustum(bbox_min, bbox_max)) {
         return CULL;
     } else if (bbox_min.z < 0 && bbox_max.z > 0) {
-        return ((rmax.x - rmin.x) < (rmax.y - rmin.y)) ? VSPLIT : HSPLIT;
+        return (((rmax.x - rmin.x) < (rmax.y - rmin.y)) ? VSPLIT : HSPLIT) & depth_mask;
     } else {
-
         float hlen=0, vlen=0;
         for (uint i = 0; i < RES; ++i) {
             float h = 0, v = 0;
@@ -131,11 +107,7 @@ uint bound (uint rpid, vec2 rmin, vec2 rmax, uint rdepth)
             vlen = max(v, vlen);
         }
 
-        if (hlen <= split_limit && vlen <= split_limit) {
-            return DRAW;
-        } else {
-            return (hlen < vlen) ? VSPLIT : HSPLIT;
-        }
+        return (hlen <= split_limit && vlen <= split_limit) ? DRAW : (((hlen < vlen) ? VSPLIT : HSPLIT) & depth_mask);
     }
 
     // unreachable
